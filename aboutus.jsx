c091806801d39e00b24e782e5124e781b98ffbe3
tharun55555 <tharungunasekaran558@gmail.com>
2026-06-19 08:58:40 +0530

    Add files via upload

---
+++ aboutus.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./AboutUs.css";
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Welcome to Paradise Nursery, your trusted destination for beautiful
+        indoor and outdoor plants. We are passionate about helping people bring
+        nature into their homes and workplaces.
+      </p>
+
+      <p>
+        Our nursery offers a wide range of carefully selected plants, including
+        flowering plants, succulents, air-purifying plants, and decorative
+        greenery.
+      </p>
+
+      <p>
+        At Paradise Nursery, we believe plants improve well-being, create
+        healthier environments, and add beauty to everyday life.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
